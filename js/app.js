93c36804ee41c79506895497e9e2995b3734c695
Diego Barletta <diegohbarletta@gmail.com>
2023-03-06 20:40:43 -0300

    changes from 6-3-23 partII

--- js/app.js
+++ js/app.js
@@ -3,10 +3,13 @@ let b = 2;
 
 console.log(a+b);
  console.log("hola");
-alert("Hola, que tal?");
-let visitante = prompt("Ingrese su nombre");
 
-if (visitante != null) {
-  document.getElementById("demo").innerHTML =
-  "Hola " + visitante + "! Bienvenido al sitio";
+let visitante = prompt("Ingrese su nombre");
+if (visitante != null ) {
+    alert("Hola, "+visitante+" que tal?");
 }
+//habia entendido mal y lo habia hecho diferente al ejercicio
+// if (visitante != "" || visitante != null) {
+//   document.getElementById("demo").innerHTML =
+//   "Hola " + visitante + "! Bienvenido al sitio";
+// }
